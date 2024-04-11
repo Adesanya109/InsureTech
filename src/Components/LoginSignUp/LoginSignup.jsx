ed1bef7e52b725391a5051af418be962c99dda7d
Taiwo Womiloju <55466974+Adesanya109@users.noreply.github.com>
2024-04-11 14:01:41 +0300

    Update to Login Page

--- src/Components/LoginSignUp/LoginSignup.jsx
+++ src/Components/LoginSignUp/LoginSignup.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
-
+import email_icon from '../CompAssets/email.png'
+import password_icon from '../CompAssets/password.png'
 
 
 const LoginSignup = () => {
+
+
+    const [action,setAction] = useState("Sign Up");
+
   return (
-    <div classname='container'>
-        <div classname="header">
-            <div classname="text">Sign Up</div>
-            <div classname="underline"></div>
+    <div className='container'>
+        <div className="header">
+            <div className="text">{action}</div>
+            <div className="underline"></div>
         </div>
-        <div classname="inputs">
-            <div classname="input">
-                <img src={user_icon} alt=""/>
-                <input type="text"/>
-            </div>
+        <div className="inputs">
 
-            <div classname="input">
+            <div className="input">
                 <img src={email_icon} alt=""/>
-                <input type="email"/>
+                <input type="email" placeholder='Email'/>
             </div>
 
-            <div classname="input">
+            <div className="input">
                 <img src={password_icon} alt=""/>
-                <input type="password"/>
+                <input type="password" placeholder='Password'/>
             </div>
 
 
+        </div>
+        <div classname="forgot-password">Forgot Password? <span>Click Here!</span></div>
+        <div className="submit-container">
+            <div className="submit">SignUp</div>
+            <div className="submit">Login</div>
         </div>
         
         
